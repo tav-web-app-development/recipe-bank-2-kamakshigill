e476380352383dff740d60ccc5ff2952970874c7
kamakshi <kamakshit2302@gmail.com>
2024-04-09 22:53:37 -0400

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import "./App.css";
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
@@ -7,6 +7,8 @@ import "./assets/style.css";
 
 function App() {
   const [recipes, setRecipes] = useState([]);
+  useMemo(() => filterRecipesComputeIntensive, []);
+
   useEffect(() => {
     fetch("https://api.sampleapis.com/recipes/recipes")
       .then((res) => {
@@ -17,20 +19,28 @@ function App() {
       });
     return () => console.log("unmounted");
   }, []);
-  function filterRecipesComputeIntensive(recipes) {
+  function filterRecipesComputeIntensive() {
     const now = performance.now();
     while (performance.now() - now < 8000) {
       //spin()
     }
-    return list.filter((word) => word.name.split(" ").length <= 4);
+    return recipes.filter((word) => word.name.split(" ").length <= 4);
   }
-  const filteredRecipes = filterRecipesComputeIntensive(recipes);
+  function deleteFromArray(id) {
+    setRecipes(recipes.filter((recipe) => recipe.id !== id));
+  }
+  // const filteredRecipes = filterRecipesComputeIntensive(recipes);
   return (
     <>
       <Navbar />
       {recipes.map((data) => (
-        <RecipeContainer recipe={data} key={data.id} />
+        <RecipeContainer
+          recipe={data}
+          key={data.id}
+          deleteFromArray={deleteFromArray}
+        />
       ))}
+
       <Footer />
     </>
   );
--- src/Components/RecipeContainer.jsx
+++ src/Components/RecipeContainer.jsx
@@ -1,5 +1,20 @@
+import { useState } from "react";
+
 /* eslint-disable react/prop-types */
-function RecipeContainer({ recipe }) {
+function RecipeContainer({ recipe, deleteFromArray }) {
+  const [editButton, setEditButton] = useState(true);
+  const editText = editButton ? "Edit" : "Save";
+  const [insertRecipe, setInsertRecipe] = useState(recipe);
+
+  // Disable ESLint for unused function deleteButton
+  // eslint-disable-next-line no-unused-vars
+  function deleteButton(id) {
+    document.getElementById(id).remove();
+  }
+  function handleChange(e, key) {
+    setInsertRecipe({ ...insertRecipe, [key]: e.target.value });
+  }
+
   return (
     <>
       <div
@@ -7,19 +22,81 @@ function RecipeContainer({ recipe }) {
         onClick={() => {
           document.title = recipe.title;
         }}
+        id={recipe.id}
       >
         <div className="recipe">
           <h2>{recipe.title}</h2>
-          <p>
-            <strong>Description:</strong>
-            {recipe.description}
-          </p>
-          <p>
-            <strong>Ingredients:</strong> {recipe.ingredients}
-          </p>
-          <p>
-            <strong>Directions:</strong> {recipe.directions}
-          </p>
+          <div>
+            <button onClick={() => setEditButton(!editButton)}>
+              {editText}
+            </button>
+            <button
+            // eslint-disable-next-line no-unused-vars
+              onClick={(e) => {
+                deleteFromArray(insertRecipe.id);
+              }}
+            >
+              Delete
+            </button>
+          </div>
+          {editButton ? (
+            <p>
+              <strong>Description:</strong>
+              {insertRecipe.description}
+            </p>
+          ) : (
+            <>
+              Description:{" "}
+              <textarea
+                rows={5}
+                cols={75}
+                type="text"
+                defaultValue={insertRecipe.description}
+                onChange={(e) => {
+                  handleChange(e, "description");
+                }}
+              />
+            </>
+          )}
+          <br />
+          {editButton ? (
+            <p>
+              <strong>Ingredients:</strong> {insertRecipe.ingredients}
+            </p>
+          ) : (
+            <>
+              Ingredients:
+              <textarea
+                rows={5}
+                cols={75}
+                type="text"
+                defaultValue={insertRecipe.ingredients}
+                onChange={(e) => {
+                  handleChange(e, "ingredients");
+                }}
+              />
+            </>
+          )}
+          <br />
+          {editButton ? (
+            <p>
+              <strong>Directions:</strong> {insertRecipe.directions}
+            </p>
+          ) : (
+            <>
+              Directions:
+              <textarea
+                rows={5}
+                cols={75}
+                type="text"
+                defaultValue={insertRecipe.directions}
+                onChange={(e) => {
+                  handleChange(e, "directions");
+                }}
+              />
+            </>
+          )}
+          <br />
           <img
             src={recipe.photoUrl}
             alt={recipe.title}
